81d5cf999930603d1f25885aa6af911d6b5cc1de
Magos-Errant <qbit666@gmail.com>
2023-03-05 23:21:55 +0900

    Now will process multiple files

--- main.py
+++ main.py
@@ -1,54 +1,62 @@
 import csv
+import os
 
-with open('#2212211_DevA1-FM7inj-FM8-NL_10K_AS03_0p5mA_offset+73p75mV_5spm4_50rdg_100plc_230217.dat', 'r') as f:
-    lines = f.readlines()
+# Define directory path
+dir_path = "./Input"
 
-# Zmienne
-sweep_number = 1
-splitted_sweeps = [[], []]
-mag = 0
-vol = 1
+# Load files from directory
+files = os.listdir(dir_path)
 
+for file in files:
+    with open(f'./Input/{file}', 'r') as f:
+        lines = f.readlines()
 
-# Function to find how many steps there are per cycle
-def find_steps():
-    a = round(splitted_sweeps[0][0])
-    b = round(splitted_sweeps[0][1])
-    step = abs(a - b)
-    steps = 2 * (a / step)-1
-    return int(abs(steps))
+    # Zmienne
+    sweep_number = 1
+    splitted_sweeps = [[], []]
+    mag = 0
+    vol = 1
 
 
-# Delete first unnecessary lines
-lines = lines[32:len(lines)]
+    # Function to find how many steps there are per cycle
+    def find_steps():
+        a = round(splitted_sweeps[0][0])
+        b = round(splitted_sweeps[0][1])
+        step = abs(a - b)
+        steps = 2 * (a / step)-1
+        return int(abs(steps))
 
-# Split into separate columns
-i = 0
-j = 0
-steps_in_cycle = 999
-for line in lines:
-    splitted_sweeps[mag].append(float(line.split(',')[4]))
-    splitted_sweeps[vol].append(float(line.split(',')[24]))
 
-    if j == 1:
-        steps_in_cycle = find_steps()
+    # Delete first unnecessary lines
+    lines = lines[32:len(lines)]
 
-    i += 1
-    j += 1
+    # Split into separate columns
+    i = 0
+    j = 0
+    steps_in_cycle = 999
+    for line in lines:
+        splitted_sweeps[mag].append(float(line.split(',')[4]))
+        splitted_sweeps[vol].append(float(line.split(',')[24]))
 
-    if i == steps_in_cycle:
-        i = 0
-        splitted_sweeps.append([])
-        splitted_sweeps.append([])
+        if j == 1:
+            steps_in_cycle = find_steps()
 
-        sweep_number += 1
-        mag += 2
-        vol += 2
+        i += 1
+        j += 1
 
-splitted_sweeps = splitted_sweeps[:-2]
+        if i == steps_in_cycle:
+            i = 0
+            splitted_sweeps.append([])
+            splitted_sweeps.append([])
 
-# Save to CSV file
-with open('output.csv', 'w', newline='') as csvfile:
-    writer = csv.writer(csvfile)
-    for row in zip(*splitted_sweeps):
-        writer.writerow(row)
+            sweep_number += 1
+            mag += 2
+            vol += 2
+
+    splitted_sweeps = splitted_sweeps[:-2]
+
+    # Save to CSV file
+    with open(f'./Output/{file[:-3]}csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        for row in zip(*splitted_sweeps):
+            writer.writerow(row)
